1ac6caa055a3aff844177566b49f47a56daa0a5c
shayal.jindal <shayal.jindal@accenture.com>
2020-05-31 18:08:27 +0800

    first commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Sample_Project_EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sample_Project_EF.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        private readonly DemoEmployeeEntities _dbContext = new DemoEmployeeEntities();
+
+        public ActionResult Index()
+        {
+            var employee = _dbContext.Employees.ToList();
+            return View(employee);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# mvc-ef-sample-project
